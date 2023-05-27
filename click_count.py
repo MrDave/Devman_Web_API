83c678b7d7200b47d4be6ebc5ad62be4dc452fe2
MrDave <davidschnoll@gmail.com>
2023-05-27 20:41:13 +0300

    Defined shorten_link function

--- click_count.py
+++ click_count.py
@@ -3,25 +3,28 @@ import os
 from dotenv import load_dotenv
 
 
-def main():
+load_dotenv()
+token = os.getenv("BIT_TOKEN")
+long_url = "https://xkcd.com/178/"
 
-    load_dotenv()
-    token = os.getenv("BIT_TOKEN")
+
+def shorten_link(token, long_url):
     url = "https://api-ssl.bitly.com/v4/shorten"
     headers = {
         "Authorization": f"Bearer {token}",
         "Content-Type": "application/json",
     }
-    data = """{
-        "long_url": "https://xkcd.com/2228",
+    data = {
+        "long_url": long_url,
         "domain": "bit.ly",
         "group_guid": "Bm46cGtZa0J"
-    }"""
-    r = requests.post(url, headers=headers, data=data)
+    }
+
+    r = requests.post(url, headers=headers, json=data)
     r.raise_for_status()
 
     print(r.json())
 
 
 if __name__ == "__main__":
-    main()
+    shorten_link(token=token, long_url=long_url)
