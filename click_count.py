81e4b06811276e32c233c9d2dfb98e57df48e9b7
MrDave <davidschnoll@gmail.com>
2023-05-27 19:59:35 +0300

    Edit to make the code shorten a link

--- click_count.py
+++ click_count.py
@@ -3,14 +3,25 @@ import os
 from dotenv import load_dotenv
 
 
-load_dotenv()
+def main():
 
-token = os.getenv("BIT_TOKEN")
-url = "https://api-ssl.bitly.com/v4/user"
-headers = {
-    "Authorization": f"Bearer {token}"
-}
-r = requests.get(url, headers=headers)
-r.raise_for_status()
+    load_dotenv()
+    token = os.getenv("BIT_TOKEN")
+    url = "https://api-ssl.bitly.com/v4/shorten"
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json",
+    }
+    data = """{
+        "long_url": "https://xkcd.com/2228",
+        "domain": "bit.ly",
+        "group_guid": "Bm46cGtZa0J"
+    }"""
+    r = requests.post(url, headers=headers, data=data)
+    r.raise_for_status()
 
-print(r.json())
+    print(r.json())
+
+
+if __name__ == "__main__":
+    main()
