068455d567c7800e1733a3bd87c4d9040a02768a
MrDave <davidschnoll@gmail.com>
2023-05-31 21:51:48 +0300

    Code cleaning
    Token and env inside def main, token name, variable names, "dry" code

--- click_count.py
+++ click_count.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 from urllib.parse import urlparse
 
 
-load_dotenv()
-token = os.getenv("BIT_TOKEN")
-
 
 def shorten_link(token, long_url):
     link_test = requests.get(long_url)
@@ -15,18 +12,15 @@ def shorten_link(token, long_url):
     url = "https://api-ssl.bitly.com/v4/shorten"
     headers = {
         "Authorization": f"Bearer {token}",
-        "Content-Type": "application/json",
     }
-    data = {
+    payload = {
         "long_url": long_url,
-        "domain": "bit.ly",
-        "group_guid": "Bm46cGtZa0J"
     }
 
-    r = requests.post(url, headers=headers, json=data)
-    r.raise_for_status()
+    response = requests.post(url, headers, payload)
+    response.raise_for_status()
 
-    return r.json()["link"]
+    return response.json()["link"]
 
 
 def count_clicks(token, bitlink):
@@ -34,8 +28,8 @@ def count_clicks(token, bitlink):
     link_test = requests.get(bitlink)
     link_test.raise_for_status()
 
-    p = urlparse(bitlink)
-    url = f"https://api-ssl.bitly.com/v4/bitlinks/{p.netloc + p.path}/clicks/summary"
+    parsed = urlparse(bitlink)
+    url = f"https://api-ssl.bitly.com/v4/bitlinks/{parsed.netloc + parsed.path}/clicks/summary"
     headers = {
         "Authorization": f"Bearer {token}",
     }
@@ -44,31 +38,35 @@ def count_clicks(token, bitlink):
         ("units", "-1"),
     )
 
-    r = requests.get(url, headers=headers, params=params)
-    r.raise_for_status()
+    response = requests.get(url, headers, params)
+    response.raise_for_status()
     
-    return r.json()["total_clicks"]
+    return response.json()["total_clicks"]
 
 
 def is_bitlink(url):
 
-    p = urlparse(url)
-    if p.netloc == "bit.ly":
+    parsed = urlparse(url)
+    if parsed.netloc == "bit.ly":
         return True
     else:
         return False
 
 
 def main():
+
+    load_dotenv()
+    token = os.getenv("BITLY_AUTH")
+
     user_link = input("Enter your link: ")
     try:
         if is_bitlink(user_link):
-            result = count_clicks(token=token, bitlink=user_link)
-            print(f"The link is a bitlink and was clicked exactly {result} time(s)")
+            clicks_total = count_clicks(token, user_link)
+            print(f"The link is a bitlink and was clicked exactly {clicks_total} time(s)")
 
         else:
-            result = shorten_link(token=token, long_url=user_link)
-            print(f"Your link wasn't a bitlink, but now it is!\nHere is it: {result}")
+            short_link = shorten_link(token, user_link)
+            print(f"Your link wasn't a bitlink, but now it is!\nHere is it: {short_link}")
 
     except requests.exceptions.HTTPError as error:
         exit(f"Invalid link: \n{error}")
